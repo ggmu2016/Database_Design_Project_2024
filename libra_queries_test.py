86042b02db376c33249ff831a45974dffcda446e
vren1 <victoriaren7@gmail.com>
2024-11-30 13:09:24 -0600

    fixed tests

--- libra_queries_test.py
+++ libra_queries_test.py
@@ -1,7 +1,6 @@
 from query_to_table import Query2TupleLarge
 from libra import libra
 import unittest
-import time
 
 class TestQueryToTable(unittest.TestCase):
     def setUp(self):
@@ -46,57 +45,63 @@ class TestQueryToTable(unittest.TestCase):
         O_pos = Query2TupleLarge(self.queries[0])
         O_neg = []
         # self.assertEqual(self.queries[0], libra(O_pos, O_neg))
-        # self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
 
     def test_query1(self): # works
         O_pos = Query2TupleLarge(self.queries[1])
         O_neg = []
         # self.assertEqual(self.queries[1], libra(O_pos, O_neg))
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
 
     def test_query2(self): # works
         O_pos = Query2TupleLarge(self.queries[2])
         O_neg = []
         # self.assertEqual(self.queries[2], libra(O_pos, O_neg))
-    
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
+
+
     def test_query3(self): # works
         O_pos = Query2TupleLarge(self.queries[3])
         O_neg = Query2TupleLarge("SELECT \"teamName\" FROM teams WHERE city!='Brandonport'")
         # self.assertEqual(self.queries[3], libra(O_pos, O_neg))
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
     
     def test_query4(self): 
         O_pos = Query2TupleLarge(self.queries[4])
         O_neg = Query2TupleLarge("SELECT players.\"playerName\", teams.\"teamName\" FROM players JOIN teams ON players.\"teamID\" = teams.\"teamID\" WHERE teams.city != 'Brandonport';")
         # self.assertEqual(self.queries[4], libra(O_pos, O_neg))
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
     
     def test_query5(self): 
         O_pos = Query2TupleLarge(self.queries[5])
-        print(self.get_O_neg_query(self.queries[5]))
         O_neg = Query2TupleLarge(self.get_O_neg_query(self.queries[5]))
         # self.assertEqual(self.queries[5], libra(O_pos, O_neg))
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
     
     def test_query6(self): # only works with distinct
         O_pos = Query2TupleLarge(self.queries[6])
         O_neg = Query2TupleLarge(self.get_O_neg_query(self.queries[6]))
-        print(O_neg)
         # self.assertEqual(self.queries[6], libra(O_pos, O_neg))
-    
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
+   
     def test_query7(self): # only works with distinct
         O_pos = Query2TupleLarge(self.queries[7])
         O_neg = Query2TupleLarge(self.get_O_neg_query(self.queries[7]))
         # self.assertEqual(self.queries[7], libra(O_pos, O_neg))
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
     
     def test_query8(self): # returns None 
         O_pos = Query2TupleLarge(self.queries[8])
         O_neg = Query2TupleLarge(self.get_O_neg_query(self.queries[8]))
-        print("O_pos: ", O_pos)
-        print("O_neg: ", O_neg)
         # self.assertEqual(self.queries[8], libra(O_pos, O_neg))
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
     
     def test_query9(self): # if the wrong predicate gets picked first (if there are multiple predicates with equal IG), the tree could generate no query
  #   fix by storing all queries with the same IG and running DTL on all maybe
         O_pos = Query2TupleLarge(self.queries[9])
         O_neg = Query2TupleLarge(self.get_O_neg_query(self.queries[9]))
         # self.assertEqual(self.queries[9], libra(O_pos, O_neg))
+        self.assertTrue(self.check_query(O_pos, O_neg, libra(O_pos, O_neg)))
 
 if __name__ == '__main__':
     unittest.main()
